0a5bcf3a39f88939daaee109b21bbb6abf060ac8
drsmile1001 <drsmile.1001@gmail.com>
2025-03-30 19:25:07 +0800

    Create user.js

---
+++ user.js
@@ -0,0 +1,32 @@
+//放置位置 C:\Users\<你的帳號>\AppData\Roaming\Mozilla\Firefox\Profiles\<亂碼>.default-release\user.js
+
+
+// ===== zh-TW (繁體中文，台灣) =====
+user_pref("font.name.monospace.zh-TW", "Maple Mono NF CN");
+user_pref("font.name.sans-serif.zh-TW", "Noto Sans CJK TC");
+user_pref("font.name.serif.zh-TW", "Noto Serif CJK TC");
+user_pref("font.default.zh-TW", "sans-serif");
+
+// ===== zh-CN (簡體中文，中國) =====
+user_pref("font.name.monospace.zh-CN", "Maple Mono NF CN");
+user_pref("font.name.sans-serif.zh-CN", "Noto Sans CJK SC");
+user_pref("font.name.serif.zh-CN", "Noto Serif CJK SC");
+user_pref("font.default.zh-CN", "sans-serif");
+
+// ===== zh-HK (繁體中文，香港) =====
+user_pref("font.name.monospace.zh-HK", "Maple Mono NF CN");
+user_pref("font.name.sans-serif.zh-HK", "Noto Sans CJK TC");
+user_pref("font.name.serif.zh-HK", "Noto Serif CJK TC");
+user_pref("font.default.zh-HK", "sans-serif");
+
+// ===== ja (日文) =====
+user_pref("font.name.monospace.ja", "Maple Mono NF CN");
+user_pref("font.name.sans-serif.ja", "Noto Sans CJK JP");
+user_pref("font.name.serif.ja", "Noto Serif CJK JP");
+user_pref("font.default.ja", "sans-serif");
+
+// ===== ko (韓文) =====
+user_pref("font.name.monospace.ko", "Maple Mono NF CN");
+user_pref("font.name.sans-serif.ko", "Noto Sans CJK KR");
+user_pref("font.name.serif.ko", "Noto Serif CJK KR");
+user_pref("font.default.ko", "sans-serif");
